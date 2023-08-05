c0198f86c7b3bc47750ff99e5bd877cb19670b49
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-08-05 19:51:37 +0100

    edit

--- src/lib/geometry.svelte
+++ src/lib/geometry.svelte
@@ -209,7 +209,7 @@
 
 				void main() {
 					vec2 position = vUv * 4.0;
-					float wave = 0.5 * (sin(position.x + time * 0.1 + 10.0 ) + mouse.x + sin(position.y + time +  mouse.y));
+					float wave = 0.5 * (tan(position.x + time * 0.1 + 10.0 ) + mouse.x + sin(position.y + time +  mouse.y));
 					vec3 color = mix(color1, color2, wave);
 					color = mix(color, color3, wave * wave);
 					gl_FragColor = vec4(color, 1.0);
@@ -217,8 +217,8 @@
 			`,
 			uniforms: {
 				color1: { value: color9 },
-				color2: { value: color9 },
-				color3: { value: color6 },
+				color2: { value: color5 },
+				color3: { value: color9 },
 				time: { value: 0 },
 				mouse: { value: mouse }
 			}
