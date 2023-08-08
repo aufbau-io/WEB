6ed51ea416896383a06c5d84de5ba74dcde6fc25
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-08-08 14:56:51 +0400

    edit

--- src/lib/geometry.svelte
+++ src/lib/geometry.svelte
@@ -207,9 +207,9 @@
 
 			`,
 			uniforms: {
-				color1: { value: color1},
+				color1: { value: color1 },
 				color2: { value: color0 },
-				color3: { value: color4 },
+				color3: { value: color5 },
 				time: { value: 0 },
 				mouse: { value: mouse }
 			}
