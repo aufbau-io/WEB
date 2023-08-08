452ed573e9d495673b2611a4bab986b3df71c824
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-08-08 17:30:43 +0400

    add fonts back

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -49,6 +49,14 @@
 		type="font/woff"
 		crossorigin="anonymous"
 	/>
+
+	<link
+	rel="preload"
+	as="font"
+	href="/fonts/NB-Architekt-Pro-Regular.woff"
+	type="font/woff"
+	crossorigin="anonymous"
+/>
 </svelte:head>
 
 <svelte:component this={Geometry} />
