ebc07304d6676f19ec0f96216f29d26c80ec15d0
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-08-08 16:23:35 +0400

    try different text bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,17 +46,61 @@
 	}
 
 	@media only screen and (max-width: 768px) {
-		.main {
-			position: absolute;
-			background: var(--background);
-			bottom: 20px;
-			width: calc(100% - 40px);
+		section {
+			padding: 20px 0;
+			justify-content: flex-end;
 		}
 
-		.main p {
-			color: var(--primary);
-			background: var(--black);
-			border: solid 1px var(--primary);
-		}
-	}
+    .main {
+        position: absolute;
+
+        width: calc(100% - 40px);
+        background: transparent; /* Make sure it's transparent */
+        padding: 20px;
+        position: relative;
+    }
+
+    .main::before, .main::after, .main p::before, .main p::after {
+        content: "";
+        position: absolute;
+        background: var(--background);
+        z-index: -1;
+    }
+
+    /* Top and Bottom boxes for .main */
+    .main::before {
+        top: 0;
+        left: 0;
+        right: 0;
+        height: 19px;
+    }
+    .main::after {
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 19px;
+    }
+
+    /* Left and Right boxes for .main */
+    .main p::before {
+        top: 1px; /* leave a 1px gap */
+        bottom: 1px; /* leave a 1px gap */
+        left: 0;
+        width: 19px;
+    }
+    .main p::after {
+        top: 1px; /* leave a 1px gap */
+        bottom: 1px; /* leave a 1px gap */
+        right: 0;
+        width: 19px;
+    }
+
+    .main p {
+        color: var(--primary);
+				background: var(--background);
+        border: none;
+        width: 100%;
+        height: 100%;
+    }
+}
 </style>
--- src/routes/sicovecas/+page.svelte
+++ src/routes/sicovecas/+page.svelte
@@ -45,18 +45,60 @@ section {
 	}
 
 	@media only screen and (max-width: 768px) {
-		.main {
-			position: absolute;
-			bottom: 20px;
-			width: calc(100% - 40px);
-			background: var(--background);
-			padding: 20px;
+		section {
+			padding: 20px 0;
+			justify-content: flex-end;
 		}
 
-		.main a {
-			color: var(--primary);
-			background: var(--black);
-			border-color: var(--primary);
-		}
-	}
+    .main {
+        position: absolute;
+
+        width: calc(100% - 40px);
+        background: transparent; /* Make sure it's transparent */
+        padding: 20px;
+        position: relative;
+    }
+
+    .main::before, .main::after, .main a::before, .main a::after {
+        content: "";
+        position: absolute;
+        background: var(--background);
+        z-index: -1;
+    }
+
+    /* Top and Bottom boxes for .main */
+    .main::before {
+        top: 0;
+        left: 0;
+        right: 0;
+        height: 19px;
+    }
+    .main::after {
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 19px;
+    }
+
+    /* Left and Right boxes for .main */
+    .main a::before {
+        top: 1px; /* leave a 1px gap */
+        bottom: 1px; /* leave a 1px gap */
+        left: 0;
+        width: 19px;
+    }
+    .main a::after {
+        top: 1px; /* leave a 1px gap */
+        bottom: 1px; /* leave a 1px gap */
+        right: 0;
+        width: 19px;
+    }
+
+    .main a {
+        color: var(--primary);
+        border: none;
+        width: 100%;
+        height: 100%;
+    }
+}
 </style>
