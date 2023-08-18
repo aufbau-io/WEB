a241d0e68dfddde6ce3fa5be6752c0f125838f49
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-08-18 16:23:38 +0400

    update again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,25 +9,25 @@
 
 	let Geometry;
 
-	function getDeviceType() {
-  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-  if ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) {
-    // This is a device which supports touch
-    if (width <= 767) {
-      // Mobile
-      return 1;
-    } else {
-      // Tablet
-      return 2;
-    }
-  } else {
-    // This is likely a laptop or desktop
-    return 3;
-  }
-}
-
 	onMount(async () => {
+
+		function getDeviceType() {
+			const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+			if ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) {
+				// This is a device which supports touch
+				if (width <= 767) {
+					// Mobile
+					return 1;
+				} else {
+					// Tablet
+					return 2;
+				}
+			} else {
+				// This is likely a laptop or desktop
+				return 3;
+			}
+		}
 
 		const module = await import('$lib/graphics/three.svelte');
 		Geometry = module.default;
