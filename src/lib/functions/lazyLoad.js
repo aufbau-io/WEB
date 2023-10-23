407f451ae46e3c3ffa0f5cacf3b34d480517eb8a
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-10-23 19:27:40 +0100

    edit

--- src/lib/functions/lazyLoad.js
+++ src/lib/functions/lazyLoad.js
@@ -6,26 +6,19 @@ let options = {
 };
 
 export const lazyLoad = (image, src) => {
-	const isPreloaded = (src) => {
-		image.style.opacity = '1';
-		return !!document.querySelector(`link[rel="preload"][href="${src}"]`);
-	};
-
 	const loaded = () => {
-		if (!isPreloaded(src)) {
-			image.style.opacity = '1';
-		}
+		// image.classList.add('visible'); // doesn't work in REPL
+		image.style.opacity = '1'; // REPL hack to apply loading animation
 	};
-
 	const observer = new IntersectionObserver((entries) => {
 		if (entries[0].isIntersecting) {
-			// console.log('an image has loaded');
-			image.src = src;
-
-			if (isPreloaded(src)) {
-				// Do nothing if the image is preloaded; it should appear instantly.
+			console.log('an image has loaded'); // console log for REPL
+			image.src = src; // replace placeholder src with the image src on observe
+			if (image.complete) {
+				// check if instantly loaded
+				loaded();
 			} else {
-				image.addEventListener('load', loaded);
+				image.addEventListener('load', loaded); // if the image isn't loaded yet, add an event listener
 			}
 		}
 	}, options);
