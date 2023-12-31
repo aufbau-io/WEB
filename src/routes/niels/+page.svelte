fe92d3388c05eba76ea663faed170e2b27ce5912
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-12-31 02:11:08 +0000

    Revert "tmp update link"
    This reverts commit accfd501cd6b051fccdb8a27bc0f2fc65679b4b5.

--- src/routes/niels/+page.svelte
+++ src/routes/niels/+page.svelte
@@ -11,7 +11,7 @@
 <section>
 	<div class="main">
 		<p>personal website</p>
-		<a href="https://daniel-niels.vercel.app/" rel="noreferrer">// visit site //</a>
+		<a href="https://danielniels.co.uk" rel="noreferrer">// visit site //</a>
 	</div>
 </section>
 
