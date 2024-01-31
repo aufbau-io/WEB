66da663ef8976875cc0979ef1867e8aedd43bd25
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2024-01-31 12:37:49 +0000

    edit site

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -6,13 +6,13 @@
 <header>
 
 	{#if $screenType==3}
-	<h1 style="padding-right:10px;">PRESS SPACEBAR</h1>
+	<h1 style="padding-right:10px;">PROJECTS (sitebuild in progress)</h1>
 	{/if}
 	
 	{#if $page.url.pathname == '/'}
 	<a href='/about'><h1 style="padding-left:10px;">ABOUT // CONTACT</h1></a>
 	{:else }
-	<a href='/'><h1 style="padding-left:10px;">HOME<span style="font-size: 20px; margin-left: 100px;">↝</span></h1></a>
+	<a href='/'><h1 style="padding-left:10px;min-width: 200px;">HOME<span style="font-size: 20px; margin-left: 100px;">↝</span></h1></a>
 	{/if}
 
 </header>
@@ -30,7 +30,7 @@
 		padding: 2px;
 		color:var(--primary);
 		background: var(--background);
-		min-width: 200px;
+		
 	}s
 
 	a:hover {
